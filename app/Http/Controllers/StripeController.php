9699ca673eb299c143288a6aea975df688cce7c3
AhmedKhaled590 <kahmed1444@gmail.com>
2022-07-07 14:55:58 +0200

    add stripe payment

---
+++ app/Http/Controllers/StripeController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Session;
+use Stripe;
+
+class StripeController extends Controller
+{
+    /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function stripe()
+    {
+        return 'stripe';
+    }
+
+    /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function stripePost(Request $request)
+    {
+        //create stripe token for payment using Stripe
+        $stripe = new \Stripe\StripeClient(
+            env('STRIPE_SECRET')
+        );
+        $token = $stripe->tokens->create([
+            'card' => [
+                'number' => $request->number,
+                'exp_month' => $request->exp_month,
+                'exp_year' => $request->exp_year,
+                'cvc' => $request->cvc,
+            ],
+        ]);
+        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+        Stripe\Charge::create([
+            "amount" => 100 * 100,
+            "currency" => "usd",
+            "source" => $token,
+            "description" => "Test payment from Khold",
+        ]);
+
+        Session::flash('success', 'Payment successful!');
+
+        return back();
+    }
+}
