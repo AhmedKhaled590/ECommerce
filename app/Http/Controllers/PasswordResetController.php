328efc5e6d6bf18d49aed3a244c2c44bdce694e0
AhmedKhaled590 <kahmed1444@gmail.com>
2022-07-10 12:09:30 +0200

    add password reset

---
+++ app/Http/Controllers/PasswordResetController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+
+class PasswordResetController extends Controller
+{
+    public function forget(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $status === Password::RESET_LINK_SENT
+        ? response()->json(['message' => 'Reset link sent'], 200)
+        : response()->json(['message' => 'Error sending reset link'], 500);
+    }
+
+    public function reset(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password),
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET
+        ? response()->json(['message' => 'Password reset successfully'], 200)
+        : response()->json(['message' => 'Error resetting password'], 500);
+    }
+}
