bbdaaea75fd657cad5a5af3565007f2312667beb
AhmedKhaled590 <kahmd1444@gmail.com>
2022-07-23 11:49:02 +0200

    add notification

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\cart;
+use App\Models\transactions;
+use App\Notifications\PaymentSuccessNotification;
 use Illuminate\Http\Request;
 use Session;
 use Stripe;
@@ -26,28 +29,47 @@ class StripeController extends Controller
     public function stripePost(Request $request)
     {
         //create stripe token for payment using Stripe
-        $stripe = new \Stripe\StripeClient(
-            env('STRIPE_SECRET')
-        );
-        $token = $stripe->tokens->create([
-            'card' => [
-                'number' => $request->number,
-                'exp_month' => $request->exp_month,
-                'exp_year' => $request->exp_year,
-                'cvc' => $request->cvc,
-            ],
-        ]);
+        try {
+            $stripe = new \Stripe\StripeClient(
+                env('STRIPE_SECRET')
+            );
+            $token = $stripe->tokens->create([
+                'card' => [
+                    'number' => $request->number,
+                    'exp_month' => $request->exp_month,
+                    'exp_year' => $request->exp_year,
+                    'cvc' => $request->cvc,
+                ],
+            ]);
 
-        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
-        Stripe\Charge::create([
-            "amount" => $request->amount * 100,
-            "currency" => $request->currency,
-            "source" => $token,
-            "description" => "Test payment from Khold",
-        ]);
+            Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+            $cart = cart::where('user_id', auth()->user()->id)->get();
+            $total = 0;
+            foreach ($cart as $item) {
+                $total += $item->price_per_quantity;
+            }
+            $response = Stripe\Charge::create([
+                "amount" => $total * 100,
+                "currency" => $request->currency,
+                "source" => $token,
+                "description" => "Test payment from Khold",
+            ]);
+            $transaction = transactions::create([
+                'stripe_id' => $response->id,
+                'user_id' => auth()->user()->id,
+                'cart_id' => $cart[0]->id,
+                'amount' => $total,
+                'currency' => $request->currency,
+                'payment_method_id' => $response->payment_method,
+                'payment_method_type' => $response->payment_method_details->card->brand,
+                'status' => $response->status,
+            ]);
+            auth()->user()->notify(new PaymentSuccessNotification());
+            Session::flash('success', 'Payment successful!');
 
-        Session::flash('success', 'Payment successful!');
-
-        return response()->json(['message' => 'Payment successful!']);
+            return response()->json(['message' => 'Payment successful!', 'transaction' => $transaction]);
+        } catch (\Exception$e) {
+            return response()->json(['message' => $e->getMessage()]);
+        }
     }
 }
---
+++ database/migrations/2022_07_23_084401_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->string('stripe_id')->primary();
+            $table->string('user_id');
+            $table->string('cart_id');
+            $table->string('amount');
+            $table->string('currency');
+            $table->string('payment_method_id');
+            $table->string('payment_method_type');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
